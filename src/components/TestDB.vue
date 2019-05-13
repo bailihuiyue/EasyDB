8f5d97cf75628b84fa1d6e6e67497a3ee1529988
Ones <964981348@qq.com>
2019-05-13 15:42:52 +0800

    完成add

--- src/components/TestDB.vue
+++ src/components/TestDB.vue
@@ -1,14 +1,38 @@
 <template>
-  <div class="hello" @click="EasyDB">测试IndexedDB</div>
+  <div>
+    <!-- <div
+      class="hello"
+      @click="EasyDB"
+    >测试EasyDB</div> -->
+    <br />
+    <br />
+    <br />
+    <div @click="testDB">测试HelloIndexedDB</div>
+    <div @click="testDB2">测试HelloIndexedDB2</div>
+    <div @click="add">测试EasyDB</div>
+  </div>
 </template>
 
 <script>
 import EasyDB from "../utils/EasyDB.js";
-
+import HDB, { HelloIndexedDB } from "../utils/hello-indexeddb.js";
 export default {
   name: "TestDB",
-  methods:{
-    EasyDB
+  methods: {
+    testDB: () => {
+      const store = new HelloIndexedDB();
+      store.setItem("name", "tomy");
+    },
+    testDB2: () => {
+      const store = new HelloIndexedDB();
+      store.setItem("name2", "tomy");
+    },
+    add: async () => {
+      // const store = EasyDB();
+      const a = await EasyDB.set("name2", Math.random());
+      const b = await EasyDB.readAll();
+      console.log(b);
+    }
   }
 };
 </script>
--- src/utils/EasyDB.js
+++ src/utils/EasyDB.js
@@ -1,8 +1,117 @@
 class EasyDB {
     constructor(options) {
-        console.log(1)
+        let { dbName = 'EasyDB', version = 1 } = options || {}
+        let DBOpenRequest = {};
+        this.inited = false;
+        this.dbName = dbName;
+        this.version = version;
+        this.db = {};
+        this.DBOpenRequest = indexedDB.open(dbName, version);
+        this.inited = this.initDB();
+    }
+
+    initDB() {
+        if (!this.inited) {
+            let that = this;
+            this.DBOpenRequest.onsuccess = (event) => {
+                that.db = that.DBOpenRequest.result;
+            };
+
+            this.DBOpenRequest.onupgradeneeded = (event) => {
+                var db = event.target.result;
+
+                db.onerror = function (event) {
+                    throw new Error(`[EasyDB]: Open DB Error`)
+                };
+
+                var objectStore = db.createObjectStore(this.dbName, {
+                    keyPath: 'key',
+                    autoIncrement: false
+                });
+                objectStore.createIndex('key', 'key');
+                objectStore.createIndex('value', 'value');
+            };
+            return true;
+        }
+    }
+
+    transaction() {
+        return this.db.transaction(this.dbName, "readwrite").objectStore(this.dbName);
+    }
+
+    async set(key, value) {
+        const val = await this.get(key);
+        if (!val) {
+            return await this.add({ key, value });
+        } else {
+            return await this.put({ key, value });
+        }
+    }
+
+    async get(key) {
+        return new Promise((resolve, reject) => {
+            const transaction = this.transaction().get(key);
+            this.dbCallback(transaction, (data) => {
+                resolve(data);
+            });
+        })
+    }
+
+    put({ key, value }) {
+        return new Promise((resolve, reject) => {
+            const transaction = this.transaction().put({ key, value });
+            this.dbCallback(transaction, (data) => {
+                resolve(data);
+            });
+        })
+    }
+
+    add(key, value) {
+        return new Promise((resolve, reject) => {
+            const transaction = this.transaction().add({ key, value });
+            this.dbCallback(transaction, (data) => {
+                resolve(data);
+            });
+        })
+    }
+
+    delete(key) {
+        return this.transaction().delete(key);
+    }
+
+    readAll() {
+        return new Promise((resolve, reject) => {
+            const transaction = this.transaction();
+            let data = [];
+            transaction.openCursor().onsuccess = function (event) {
+                const cursor = event.target.result;
+                if (cursor) {
+                    data.push(cursor.value);
+                    cursor.continue();
+                } else {
+                    resolve(data);
+                }
+            };
+        })
+    }
+
+    dbCallback(transaction, cb) {
+        transaction.onsuccess = (event) => {
+            cb(event.target.result.value || event.target.result);
+        };
+        transaction.onerror = (event) => {
+            throw new Error(`${this.dbName} Error:${event.target.error}`);
+        }
+    }
+    promise(fun) {
+        return new Promise((resolve, reject) => {
+            fun(resolve, reject);
+        });
     }
 }
 
-const db = (options) => new EasyDB(options)
-export default db
+let DB = null;
+if (!DB) {
+    DB = new EasyDB()
+}
+export default DB
